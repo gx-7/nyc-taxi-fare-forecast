d22526ef0a990b93fb1c4f82d171553ebfa6ad7a
Guillaume <guillaume.ostrom@centraliens.net>
2018-10-04 12:29:55 -0400

    A linear analysis for the Google Cloud challenge
    -first order fare/distance relation
    -night/day fare
    -airport flat fare 
    Result : First try for this challenge with a linear model. the score will be improve with nn/time series predictions.

---
+++ main/linear-analysis.py
@@ -0,0 +1,419 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+% matplotlib inline
+
+df_train =  pd.read_csv('../input/train.csv', nrows = 2_000_000, parse_dates=["pickup_datetime"])
+
+df_train.dtypes
+df_train.head()
+df_train.describe()
+
+# plot repartition function : number over fare in USD
+df_train = df_train[df_train.fare_amount>=0]
+df_train[df_train.fare_amount<100].fare_amount.hist(bins=100, figsize=(14,3))
+plt.xlabel('fare $USD')
+plt.title('Histogram');
+
+
+
+min(df_test.pickup_longitude.min(), df_test.dropoff_longitude.min()), \
+max(df_test.pickup_longitude.max(), df_test.dropoff_longitude.max())
+
+min(df_test.pickup_latitude.min(), df_test.dropoff_latitude.min()), \
+max(df_test.pickup_latitude.max(), df_test.dropoff_latitude.max())
+
+def select_within_boundingbox(df, BB):
+    return (df.pickup_longitude >= BB[0]) & (df.pickup_longitude <= BB[1]) & \
+           (df.pickup_latitude >= BB[2]) & (df.pickup_latitude <= BB[3]) & \
+           (df.dropoff_longitude >= BB[0]) & (df.dropoff_longitude <= BB[1]) & \
+           (df.dropoff_latitude >= BB[2]) & (df.dropoff_latitude <= BB[3])
+            
+BB = (-74.5, -72.8, 40.5, 41.8)
+nyc_map = plt.imread('https://aiblog.nl/download/nyc_-74.5_-72.8_40.5_41.8.png')
+
+BB_zoom = (-74.3, -73.7, 40.5, 40.9)
+nyc_map_zoom = plt.imread('https://aiblog.nl/download/nyc_-74.3_-73.7_40.5_40.9.png')
+
+df_train = df_train[select_within_boundingbox(df_train, BB)]
+
+def plot_on_map(df, BB, nyc_map, s=10, alpha=0.2):
+    fig, axs = plt.subplots(1, 2, figsize=(16,10))
+    axs[0].scatter(df.pickup_longitude, df.pickup_latitude, zorder=1, alpha=alpha, c='r', s=s)
+    axs[0].set_xlim((BB[0], BB[1]))
+    axs[0].set_ylim((BB[2], BB[3]))
+    axs[0].set_title('Pickup locations')
+    axs[0].imshow(nyc_map, zorder=0, extent=BB)
+
+    axs[1].scatter(df.dropoff_longitude, df.dropoff_latitude, zorder=1, alpha=alpha, c='r', s=s)
+    axs[1].set_xlim((BB[0], BB[1]))
+    axs[1].set_ylim((BB[2], BB[3]))
+    axs[1].set_title('Dropoff locations')
+    axs[1].imshow(nyc_map, zorder=0, extent=BB)
+    
+  plot_on_map(df_train, BB, nyc_map, s=1, alpha=0.3)
+  
+  plot_on_map(df_train, BB_zoom, nyc_map_zoom, s=1, alpha=0.3)
+  
+  plot_on_map(df_test, BB, nyc_map, alpha=1.0, s=20)
+  
+  def plot_hires(df, BB, figsize=(12, 12), ax=None, c=('r', 'b')):
+    if ax == None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    idx = select_within_boundingbox(df, BB)
+    ax.scatter(df[idx].pickup_longitude, df[idx].pickup_latitude, c=c[0], s=0.01, alpha=0.5)
+    ax.scatter(df[idx].dropoff_longitude, df[idx].dropoff_latitude, c=c[1], s=0.01, alpha=0.5)
+    
+    
+plot_hires(df_train, (-74.1, -73.7, 40.6, 40.9))
+plot_hires(df_train, (-74, -73.95, 40.7, 40.8))
+
+
+def distance(lat1, lon1, lat2, lon2):
+    p = 0.017453292519943295 # Pi/180
+    a = 0.5 - np.cos((lat2 - lat1) * p)/2 + np.cos(lat1 * p) * np.cos(lat2 * p) * (1 - np.cos((lon2 - lon1) * p)) / 2
+    return 0.6213712 * 12742 * np.arcsin(np.sqrt(a)) # 2*R*asin...
+    
+n_lon, n_lat = 200, 200 # number of grid bins per longitude, latitude dimension
+density_pickup, density_dropoff = np.zeros((n_lat, n_lon)), np.zeros((n_lat, n_lon)) # prepare arrays
+
+bins_lon = np.zeros(n_lon+1) # bin
+bins_lat = np.zeros(n_lat+1) # bin
+delta_lon = (BB[1]-BB[0]) / n_lon # bin longutide width
+delta_lat = (BB[3]-BB[2]) / n_lat # bin latitude height
+bin_width_miles = distance(BB[2], BB[1], BB[2], BB[0]) / n_lon # bin width in miles
+bin_height_miles = distance(BB[3], BB[0], BB[2], BB[0]) / n_lat # bin height in miles
+for i in range(n_lon+1):
+    bins_lon[i] = BB[0] + i * delta_lon
+for j in range(n_lat+1):
+    bins_lat[j] = BB[2] + j * delta_lat
+
+inds_pickup_lon = np.digitize(df_train.pickup_longitude, bins_lon)
+inds_pickup_lat = np.digitize(df_train.pickup_latitude, bins_lat)
+inds_dropoff_lon = np.digitize(df_train.dropoff_longitude, bins_lon)
+inds_dropoff_lat = np.digitize(df_train.dropoff_latitude, bins_lat)
+
+dxdy = bin_width_miles * bin_height_miles
+for i in range(n_lon):
+    for j in range(n_lat):
+        density_pickup[j, i] = np.sum((inds_pickup_lon==i+1) & (inds_pickup_lat==(n_lat-j))) / dxdy
+        density_dropoff[j, i] = np.sum((inds_dropoff_lon==i+1) & (inds_dropoff_lat==(n_lat-j))) / dxdy
+        
+        
+fig, axs = plt.subplots(2, 1, figsize=(18, 24))
+axs[0].imshow(nyc_map, zorder=0, extent=BB);
+im = axs[0].imshow(np.log1p(density_pickup), zorder=1, extent=BB, alpha=0.6, cmap='plasma')
+axs[0].set_title('Pickup density [datapoints per sq mile]')
+cbar = fig.colorbar(im, ax=axs[0])
+cbar.set_label('log(1 + #datapoints per sq mile)', rotation=270)
+
+axs[1].imshow(nyc_map, zorder=0, extent=BB);
+im = axs[1].imshow(np.log1p(density_dropoff), zorder=1, extent=BB, alpha=0.6, cmap='plasma')
+axs[1].set_title('Dropoff density [datapoints per sq mile]')
+cbar = fig.colorbar(im, ax=axs[1])
+cbar.set_label('log(1 + #datapoints per sq mile)', rotation=270)
+
+
+df_train['year'] = df_train.pickup_datetime.apply(lambda t: t.year)
+df_train['weekday'] = df_train.pickup_datetime.apply(lambda t: t.weekday())
+df_train['hour'] = df_train.pickup_datetime.apply(lambda t: t.hour)
+
+
+n_hours = 24
+n_weekdays = 7
+n_years = 7
+n_bins_lon = 30
+n_bins_lat = 30
+
+BB_traffic = (-74.025, -73.925, 40.7, 40.8)
+
+def calculate_trafic_density(df):
+    traffic = np.zeros((n_years, n_weekdays, n_hours, n_bins_lat, n_bins_lon))
+
+    bins_lon = np.zeros(n_bins_lon+1) # bin
+    bins_lat = np.zeros(n_bins_lat+1) # bin
+    
+    delta_lon = (BB_traffic[1]-BB_traffic[0]) / n_bins_lon # bin longutide width
+    delta_lat = (BB_traffic[3]-BB_traffic[2]) / n_bins_lat # bin latitude height
+    
+    for i in range(n_bins_lon+1):
+        bins_lon[i] = BB_traffic[0] + i * delta_lon
+    for j in range(n_bins_lat+1):
+        bins_lat[j] = BB_traffic[2] + j * delta_lat
+    
+    for y in range(n_years):
+        for d in range(n_weekdays):
+            for h in range(n_hours):
+                idx = (df.year==(2009+y)) & (df.weekday==d) & (df.hour==h)
+
+                # Digitize per longitude, latitude dimension
+                inds_pickup_lon = np.digitize(df[idx].pickup_longitude, bins_lon)
+                inds_pickup_lat = np.digitize(df[idx].pickup_latitude, bins_lat)
+
+                for i in range(n_bins_lon):
+                    for j in range(n_bins_lat):
+                        traffic[y, d, h, j, i] = traffic[y, d, h, j, i] + \
+                                                 np.sum((inds_pickup_lon==i+1) & (inds_pickup_lat==j+1))
+    
+    return traffic 
+
+def plot_traffic(traffic, y, d):
+    days = {'monday' : 0, 'tuesday' : 1, 'wednesday' : 2, 'thursday' : 3, 'friday' : 4, 'saturday' : 5, 'sunday' : 6}
+    fig, axs = plt.subplots(3,8,figsize=(18,7))
+    axs = axs.ravel()
+    for h in range(24):
+        axs[h].imshow(traffic[y-2009,days[d],h,::-1,:], zorder=1, cmap='coolwarm', clim=(0, traffic.max()))
+        axs[h].get_xaxis().set_visible(False)
+        axs[h].get_yaxis().set_visible(False)
+        axs[h].set_title('h={}'.format(h))
+    fig.suptitle("Pickup traffic density, year={}, day={} (max_pickups={})".format(y, d, traffic.max()))
+    
+    
+    
+traffic = calculate_trafic_density(df_train)
+
+plot_traffic(traffic, 2009, 'monday')
+plot_traffic(traffic, 2009, 'friday')
+plot_traffic(traffic, 2009, 'sunday')
+
+plot_traffic(traffic, 2014, 'monday')
+plot_traffic(traffic, 2014, 'friday')
+plot_traffic(traffic, 2014, 'sunday')
+
+df_train['distance_miles'] = distance(df_train.pickup_latitude, df_train.pickup_longitude, \
+                                      df_train.dropoff_latitude, df_train.dropoff_longitude)
+
+df_train.distance_miles.hist(bins=50, figsize=(12,4))
+plt.xlabel('distance miles')
+plt.title('Histogram ride distances in miles')
+df_train.distance_miles.describe()
+
+
+df_train.groupby('passenger_count')['distance_miles', 'fare_amount'].mean()
+
+print("Average $USD/Mile : {:0.2f}".format(df_train.fare_amount.sum()/df_train.distance_miles.sum()))
+
+
+fig, axs = plt.subplots(1, 2, figsize=(16,6))
+axs[0].scatter(df_train.distance_miles, df_train.fare_amount, alpha=0.2)
+axs[0].set_xlabel('distance mile')
+axs[0].set_ylabel('fare $USD')
+axs[0].set_title('All data')
+
+idx = (df_train.distance_miles < 15) & (df_train.fare_amount < 100)
+axs[1].scatter(df_train[idx].distance_miles, df_train[idx].fare_amount, alpha=0.2)
+axs[1].set_xlabel('distance mile')
+axs[1].set_ylabel('fare $USD')
+axs[1].set_title('Zoom in on distance < 15 mile, fare < $100');
+
+
+jfk = (-73.7822222222, 40.6441666667)
+nyc = (-74.0063889, 40.7141667)
+
+def plot_location_fare(loc, name, range=1.5):
+    fig, axs = plt.subplots(1, 2, figsize=(14, 5))
+    idx = (distance(df_train.pickup_latitude, df_train.pickup_longitude, loc[1], loc[0]) < range)
+    df_train[idx].fare_amount.hist(bins=100, ax=axs[0])
+    axs[0].set_xlabel('fare $USD')
+    axs[0].set_title('Histogram pickup location within {} miles of {}'.format(range, name))
+
+    idx = (distance(df_train.dropoff_latitude, df_train.dropoff_longitude, loc[1], loc[0]) < range)
+    df_train[idx].fare_amount.hist(bins=100, ax=axs[1])
+    axs[1].set_xlabel('fare $USD')
+    axs[1].set_title('Histogram dropoff location within {} miles of {}'.format(range, name));
+    
+plot_location_fare(jfk, 'JFK Airport')
+
+ewr = (-74.175, 40.69) 
+lgr = (-73.87, 40.77) 
+plot_location_fare(ewr, 'Newark Airport')
+plot_location_fare(lgr, 'LaGuardia Airport')
+
+df_train['fare_per_mile'] = df_train.fare_amount / df_train.distance_miles
+df_train.fare_per_mile.describe()
+
+idx = (df_train.distance_miles < 3) & (df_train.fare_amount < 100)
+plt.scatter(df_train[idx].distance_miles, df_train[idx].fare_per_mile)
+plt.xlabel('distance mile')
+plt.ylabel('fare per distance mile')
+
+# theta here is estimated by hand
+theta = (16, 4.0)
+x = np.linspace(0.1, 3, 50)
+plt.plot(x, theta[0]/x + theta[1], '--', c='r', lw=2
+
+df_train.pivot_table('fare_per_mile', index='hour', columns='year').plot(figsize=(14,6))
+plt.ylabel('Fare $USD / mile');
+
+from sklearn.linear_model import LinearRegression
+
+# plot all years
+for year in df_train.year.unique():
+    # create figure
+    fig, axs = plt.subplots(4, 6, figsize=(18, 10))
+    axs = axs.ravel()
+    
+    # plot for all hours
+    for h in range(24):
+        idx = (df_train.distance_miles < 15) & (df_train.fare_amount < 100) & (df_train.hour == h) & \
+              (df_train.year == year)
+        axs[h].scatter(df_train[idx].distance_miles, df_train[idx].fare_amount, alpha=0.2, s=1)
+        axs[h].set_xlabel('distance miles')
+        axs[h].set_ylabel('fare $USD')
+        axs[h].set_xlim((0, 15))
+        axs[h].set_ylim((0, 70))
+
+        model = LinearRegression(fit_intercept=False)
+        x, y = df_train[idx].distance_miles.values.reshape(-1,1), df_train[idx].fare_amount.values.reshape(-1,1)
+        X = np.concatenate((np.ones(x.shape), x), axis=1)
+        model.fit(X, y)
+        xx = np.linspace(0.1, 25, 100)
+        axs[h].plot(xx, model.coef_[0][0] + xx * model.coef_[0][1], '--', c='r', lw=2)
+        axs[h].set_title('hour = {}, theta=({:0.2f},{:0.2f})'.format(h, model.coef_[0][0], model.coef_[0][1]))
+
+    plt.suptitle("Year = {}".format(year))
+    plt.tight_layout(rect=[0, 0, 1, 0.95]);
+    
+    df_train['distance_to_center'] = distance(nyc[1], nyc[0], df_train.pickup_latitude, df_train.pickup_longitude)
+    
+    fig, axs = plt.subplots(1, 2, figsize=(16,6))
+im = axs[0].scatter(df_train.distance_to_center, df_train.distance_miles, c=np.clip(df_train.fare_amount, 0, 100), 
+                     cmap='viridis', alpha=1.0, s=1)
+axs[0].set_xlabel('pickup distance from NYC center')
+axs[0].set_ylabel('distance miles')
+axs[0].set_title('All data')
+cbar = fig.colorbar(im, ax=axs[0])
+cbar.ax.set_ylabel('fare_amount', rotation=270)
+
+idx = (df_train.distance_to_center < 15) & (df_train.distance_miles < 35)
+im = axs[1].scatter(df_train[idx].distance_to_center, df_train[idx].distance_miles, 
+                     c=np.clip(df_train[idx].fare_amount, 0, 100), cmap='viridis', alpha=1.0, s=1)
+axs[1].set_xlabel('pickup distance from NYC center')
+axs[1].set_ylabel('distance miles')
+axs[1].set_title('Zoom in')
+cbar = fig.colorbar(im, ax=axs[1])
+cbar.ax.set_ylabel('fare_amount', rotation=270);
+
+df_train['pickup_distance_to_jfk'] = distance(jfk[1], jfk[0], df_train.pickup_latitude, df_train.pickup_longitude)
+df_train['dropoff_distance_to_jfk'] = distance(jfk[1], jfk[0], df_train.dropoff_latitude, df_train.dropoff_longitude)
+
+idx = ~((df_train.pickup_distance_to_jfk < 1) | (df_train.dropoff_distance_to_jfk < 1))
+
+fig, axs = plt.subplots(1, 2, figsize=(16,6))
+im = axs[0].scatter(df_train[idx].distance_to_center, df_train[idx].distance_miles, 
+                    c=np.clip(df_train[idx].fare_amount, 0, 100), 
+                     cmap='viridis', alpha=1.0, s=1)
+axs[0].set_xlabel('pickup distance from NYC center')
+axs[0].set_ylabel('distance miles')
+axs[0].set_title('All data')
+cbar = fig.colorbar(im, ax=axs[0])
+cbar.ax.set_ylabel('fare_amount', rotation=270)
+
+idx1 = idx & (df_train.distance_to_center < 15) & (df_train.distance_miles < 35)
+im = axs[1].scatter(df_train[idx1].distance_to_center, df_train[idx1].distance_miles, 
+                     c=np.clip(df_train[idx1].fare_amount, 0, 100), cmap='viridis', alpha=1.0, s=1)
+axs[1].set_xlabel('pickup distance from NYC center')
+axs[1].set_ylabel('distance miles')
+axs[1].set_title('Zoom in')
+cbar = fig.colorbar(im, ax=axs[1])
+cbar.ax.set_ylabel('fare_amount', rotation=270);
+
+idx = (df_train.fare_amount>80) & (df_train.distance_miles<35) 
+plot_on_map(df_train[idx], BB, nyc_map)
+
+df_train['pickup_distance_to_ewr'] = distance(ewr[1], ewr[0], df_train.pickup_latitude, df_train.pickup_longitude)
+df_train['dropoff_distance_to_ewr'] = distance(ewr[1], ewr[0], df_train.dropoff_latitude, df_train.dropoff_longitude)
+df_train['pickup_distance_to_lgr'] = distance(lgr[1], lgr[0], df_train.pickup_latitude, df_train.pickup_longitude)
+df_train['dropoff_distance_to_lgr'] = distance(lgr[1], lgr[0], df_train.dropoff_latitude, df_train.dropoff_longitude)
+
+df_test['distance_miles'] = distance(df_test.pickup_latitude, df_test.pickup_longitude, \
+                                     df_test.dropoff_latitude, df_test.dropoff_longitude)
+df_test['distance_to_center'] = distance(nyc[1], nyc[0], \
+                                          df_test.dropoff_latitude, df_test.dropoff_longitude)
+df_test['hour'] = df_test.pickup_datetime.apply(lambda t: pd.to_datetime(t).hour)
+df_test['year'] = df_test.pickup_datetime.apply(lambda t: pd.to_datetime(t).year)
+
+idx = (df_train.distance_to_center<15) & (df_train.passenger_count!=0)
+features = ['year', 'hour', 'distance_miles', 'passenger_count']
+X = df_train[idx][features].values
+y = df_train[idx]['fare_amount'].values
+
+
+X.shape, y.shape
+
+from sklearn.metrics import mean_squared_error, explained_variance_score
+
+def plot_prediction_analysis(y, y_pred, figsize=(10,4), title=''):
+    fig, axs = plt.subplots(1, 2, figsize=figsize)
+    axs[0].scatter(y, y_pred)
+    mn = min(np.min(y), np.min(y_pred))
+    mx = max(np.max(y), np.max(y_pred))
+    axs[0].plot([mn, mx], [mn, mx], c='red')
+    axs[0].set_xlabel('$y$')
+    axs[0].set_ylabel('$\hat{y}$')
+    rmse = np.sqrt(mean_squared_error(y, y_pred))
+    evs = explained_variance_score(y, y_pred)
+    axs[0].set_title('rmse = {:.2f}, evs = {:.2f}'.format(rmse, evs))
+    
+    axs[1].hist(y-y_pred, bins=50)
+    avg = np.mean(y-y_pred)
+    std = np.std(y-y_pred)
+    axs[1].set_xlabel('$y - \hat{y}$')
+    axs[1].set_title('Histrogram prediction error, $\mu$ = {:.2f}, $\sigma$ = {:.2f}'.format(avg, std))
+    
+    if title!='':
+        fig.suptitle(title)
+        
+        
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+
+model_lin = Pipeline((
+        ("standard_scaler", StandardScaler()),
+        ("lin_reg", LinearRegression()),
+    ))
+model_lin.fit(X_train, y_train)
+
+y_train_pred = model_lin.predict(X_train)
+plot_prediction_analysis(y_train, y_train_pred, title='Linear Model - Trainingset')
+
+y_test_pred = model_lin.predict(X_test)
+plot_prediction_analysis(y_test, y_test_pred, title='Linear Model - Testset')
+
+def plot_rmse_analysis(model, X, y, N=400, test_size=0.25, figsize=(10,4), title=''):
+    rmse_train, rmse_test = [], []
+    for i in range(N):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+
+        model.fit(X_train, y_train)
+        y_train_pred = model.predict(X_train)
+        y_test_pred = model.predict(X_test)
+
+        rmse_train.append(np.sqrt(mean_squared_error(y_train, y_train_pred)))
+        rmse_test.append(np.sqrt(mean_squared_error(y_test, y_test_pred)))
+
+    g = sns.jointplot(np.array(rmse_train), np.array(rmse_test), kind='scatter', stat_func=None, size=5)
+    g.set_axis_labels("RMSE training ($\mu$={:.2f})".format(np.mean(rmse_train)), 
+                      "RMSE test ($\mu$={:.2f})".format(np.mean(rmse_test)))
+    plt.subplots_adjust(top=0.9)
+    g.fig.suptitle('{} (N={}, test_size={:0.2f})'.format(title, N, test_size))
+    
+    plot_rmse_analysis(model_lin, X[:100_000,:], y[:100_000], title='Linear model')
+    
+    XTEST = df_test[features].values
+    
+    filename = './output/baseline_linear'
+
+y_pred_final = model_lin.predict(XTEST)
+
+submission = pd.DataFrame(
+    {'key': df_test.key, 'fare_amount': y_pred_final},
+    columns = ['key', 'fare_amount'])
+submission.to_csv('submission.csv', index = False)
